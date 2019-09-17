e63de21487e23a3cbe26ad7215f70379218b3251
Austin Every <austin.every@oliveai.com>
2019-09-17 08:40:33 -0400

    Owl app update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-
 // function App() {
 //   return (
 //     <div className="App">
@@ -19,18 +18,18 @@ import './App.css';
 const owl = {
   title: 'Excellent Owl',
   src: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg',
-  height: 100,
-  width:100
+  height: 200,
+  width:200
 };
 
 // Component class starts here:
 class Owl extends React.Component {
   render() {
     return (
-      <div> 
+      <div className = "parentDiv">  
         <h1>{owl.title}</h1>
         <img alt={owl.title} src={owl.src} width={owl.width} height={owl.height} />
-        <textarea></textarea>
+        <textarea className = "textArea"></textarea>
       </div>
     );
   }
