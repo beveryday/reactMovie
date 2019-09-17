59c4711a241f9ff4a020669bafcc3661a88466e4
Austin Every <austin.every@oliveai.com>
2019-09-17 08:26:22 -0400

    good owl

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+
+const owl = {
+  title: 'Excellent Owl',
+  src: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg',
+  height: 100,
+  width:100
+};
+
+// Component class starts here:
+class Owl extends React.Component {
+  render() {
+    return (
+      <div> 
+        <h1>{owl.title}</h1>
+        <img alt={owl.title} src={owl.src} width={owl.width} height={owl.height} />
+        <textarea></textarea>
+      </div>
+    );
+  }
+};
+
+export default Owl;
