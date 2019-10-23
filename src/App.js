08e6ff3bef21ec9ae118a7b90c37e2924904075d
Austin Every <austin.every@oliveai.com>
2019-10-23 18:38:57 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import Card from '@material-ui/core/Card';
 // import logo from './logo.svg';
 import './App.css';
 import clsx from 'clsx';
+
+import Rating from '@material-ui/lab/Rating';
 import Logo from './14026976_f520.jpg';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-
+import Star from './star.js';
 const clip = {
   title: 'Back to the Future',
   src: './img/14026976_f520.jpg',
@@ -47,7 +40,7 @@ class Movie extends React.Component {
       /> 
         <h1>{clip.title}</h1>
         <img alt={clip.title} src={Logo} width={clip.width} height={clip.height} />
-        
+       <Star />
       </Card>
     );
   }
