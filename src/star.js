32efec60f6c983f5df5078ce2e21364541266c1c
Austin Every <austin.every@oliveai.com>
2019-10-24 13:31:48 -0400

    upload star

--- src/star.js
+++ src/star.js
@@ -1,21 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
-export default function SimpleRating() {
-  const [value, setValue] = React.useState(2);
+const labels = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+};
+
+function IconContainer(props) {
+  const { value, ...other } = props;
+  return (
+    <Tooltip title={labels[value] || ''}>
+      <div {...other} />
+    </Tooltip>
+  );
+}
+
+IconContainer.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles({
+  rating1: {
+    width: 200,
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+export default function HoverRating() {
+  const value = 2;
+  const [hover, setHover] = React.useState(-1);
+  const classes = useStyles();
 
   return (
     <div>
       <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Controlled</Typography>
+        <Typography component="legend">Rate this movie!</Typography>
         <Rating
-          name="simple-controlled"
+          name="hover-tooltip"
           value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
+          precision={0.5}
+          IconContainerComponent={IconContainer}
         />
       </Box>
     </div>
